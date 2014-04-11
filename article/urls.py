efac5a3ce7099953b2e9e353fc3352e97a31ea0f
Suchet Bargoti <bargoti.suchet@gmail.com>
2014-04-11 21:04:19 +1000

    Finished tutorial 16 webservices

--- article/urls.py
+++ article/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from api import ArticleResource
+
+article_resource = ArticleResource()
 
 urlpatterns = patterns('',
     url(r'^all/$','article.views.articles'),
@@ -7,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^create/$', 'article.views.create'),
     url(r'^like/(?P<article_id>\d+)/$', 'article.views.like_article'),
     url(r'^search/$', 'article.views.search_titles'),
+    url(r'^api/', include(article_resource.urls)),
 )
