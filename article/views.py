46fb957e8493f6779e69f0f89d21ae0c03a8327c
Suchet Bargoti <bargoti.suchet@gmail.com>
2014-04-11 18:23:32 +1000

    Finished tutorial 8, skipping to tutorial 12

--- article/views.py
+++ article/views.py
@@ -4,17 +4,35 @@ from django.shortcuts import render_to_response
 # from django.template import Context
 # from django.views.generic.base import TemplateView
 from article.models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 def articles(request):
+    language = 'en-gb'
+    session_language = 'en-gb'
+    
+    if 'lang' in request.COOKIES:
+        language = request.COOKIES['lang']
+    
+    if 'lang' in request.session:
+        session_language = request.session['lang']
+        
     return render_to_response('articles.html',
-                              {'articles' : Article.objects.all() })
+                              {'articles' : Article.objects.all(),
+                               'language' : language,
+                               'session_language' : session_language } )
 
 def article(request,article_id=1):
     return render_to_response('article.html',
                               {'article': Article.objects.get(id=article_id) })
                               
-
+def language(request, language='en-gb'):
+    response = HttpResponse("setting language to %s" % language)
+    
+    response.set_cookie('lang', language) 
+    
+    request.session['lang'] = language
+    return response
 ########### FROM TUTORIAL 3 ###########
 #def hello(request):
 #    name = "Annie"
